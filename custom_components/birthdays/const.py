eff9752f7a4d547fd3514653d16919aac3d051ab
Dennis M <68971788+UnoSite@users.noreply.github.com>
2025-03-17 08:23:35 +0100

    Update const.py

--- custom_components/birthdays/const.py
+++ custom_components/birthdays/const.py
@@ -22,3 +22,14 @@ BINARY_SENSOR_NAME_TEMPLATE = "binary_sensor.birthdays_{name}_today"
 
 # Device identifiers
 DEVICE_ID_TEMPLATE = "birthdays_{name}"
+
+# Manufacturer & Model
+MANUFACTURER = "UnoSite"
+MODEL = "Birthdays Integration"
+
+# Icons
+ICON_BIRTHDAY = "mdi:cake-variant"
+ICON_NEXT_BIRTHDAY = "mdi:calendar-clock"
+ICON_DATE_OF_BIRTH = "mdi:calendar"
+ICON_YEARS_OLD = "mdi:numeric"
+ICON_BINARY_SENSOR = "mdi:cake"
